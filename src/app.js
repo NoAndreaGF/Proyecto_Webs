975d8637eb4fffffebd650e1059ede292a17e142
andrea <andrea.garcia216626@potros.itson.edu.mx>
2022-11-12 17:33:16 -0700

    Token

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(function(req, res, next) {
-  //res.header("x-access-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFuYSBCb2RlZ2EiLCJwYXNzd29yZCI6IjEyMzQ1NiIsImlhdCI6MTY2ODExNzQxNiwiZXhwIjoxNjY4OTgxNDE2fQ.t49HBIF1JSQgHGZuxlTDM5UiQog1tfI3hlF3vvNqYBI");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
